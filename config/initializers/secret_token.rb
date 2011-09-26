e580194243d53a8b9b996a2baca627021e141715
Nathan Wise <nathanwise@gmail.com>
2011-09-25 21:37:59 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChipotleFamily::Application.config.secret_token = '09f4ff7ecfa859c754caa55dc78ff02dd21a0e93b95b476b18218e6e8d35a9abafd8940a4a021a36a29b5c3eb776536ba9dfda8f6dd357bf2aeb3b8c296f0662'
